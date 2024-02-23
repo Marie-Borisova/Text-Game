5b57466358004982d8e181957c66264768e77e93
Marie-Borisova <161057813+Marie-Borisova@users.noreply.github.com>
2024-02-23 17:12:05 +0100

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+       WorldCreator wc = new WorldCreator();
+        wc.readInputFromFile();
+        wc.traversingThroughRooms();
+
+
+        
+
+
+    }
+}
